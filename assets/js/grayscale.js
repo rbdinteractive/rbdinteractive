b37e4b31f3be05292700fa4ea204ff496c99ca15
DESKTOP-15QVNIU\Renderbydavid2060RTXS <renderbydavid@yahoo.com>
2023-03-29 16:30:44 -0500

    no message

--- assets/js/grayscale.js
+++ assets/js/grayscale.js
@@ -1,22 +1,6 @@
 (function() {
   "use strict"; // Start of use strict
 
-  var scrollToTop = document.querySelector('.scroll-to-top');
-  
-  if (scrollToTop) {
-    
-    // Scroll to top button appear
-    window.addEventListener('scroll', function() {
-      var scrollDistance = window.pageYOffset;
-
-      if (scrollDistance > 100) {
-        scrollToTop.style.display = 'block';
-      } else {
-        scrollToTop.style.display = 'none';
-      }
-    });
-  }
-
   var mainNav = document.querySelector('#mainNav');
 
   if (mainNav) {
@@ -24,7 +8,7 @@
     var navbarCollapse = mainNav.querySelector('.navbar-collapse');
     
     if (navbarCollapse) {
-      
+
       var collapse = new bootstrap.Collapse(navbarCollapse, {
         toggle: false
       });
